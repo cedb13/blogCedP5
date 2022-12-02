50c31662be9f49e0ff9fd8191f63a5fdf51f869d
cedb13 <93099582+cedb13@users.noreply.github.com>
2022-12-02 05:45:56 +0100

    Delete App directory

--- App/views/templates/error.php
+++
@@ -1,8 +0,0 @@
-<?php $title = "Le blog du Dev Junior by Cédric"; ?>
-
-<?php ob_start(); ?>
-<h1>Le blog du Dev Junior by Cédric !</h1>
-<p>Une erreur est survenue : <?= $errorMessage ?></p>
-<?php $content = ob_get_clean(); ?>
-
-<?php require('layout.php') ?>
